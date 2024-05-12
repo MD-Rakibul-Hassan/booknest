4c91c53568626c1644acfe89c089c1180b9d0710
Rakibul-Hassan-Shehab <mdrakibulhassan2425@gmail.com>
2024-05-12 12:46:33 +0600

    login button style added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,11 +5,20 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 const Login = () => {
     const provider = new GoogleAuthProvider()
     const handleLogin = (e) => {
-        signInWithPopup(auth,provider)
+        signInWithPopup(auth, provider)
+            .then(data => {
+            console.log(data)
+            })
+            .catch(error => console.log(error))  
     }
   return (
-      <div>
-          <button onClick={handleLogin}>Login With Google</button>
+      <div className='h-screen flex justify-center items-center'>
+          <button
+              onClick={handleLogin}
+              className='bg-lime-600 px-4 py-2 rounded-xl text-white border-4 hover:bg-transparent hover:text-black'
+          >
+              Login With Google
+          </button>
     </div>
   )
 }
